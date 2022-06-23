1aa9d2f90cac8f9cbd0e0f12aab11fcae4e0d612
rschragger <80379457+rschragger@users.noreply.github.com>
2022-06-23 16:08:47 +1000

    Working out/playing with jQuery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 //Create variables ---------------------------------------------------------
 //var IDs
 //defaults
-var defaultVals = {
-}
+// var defaultVals = {
+
+// }
 
 //Other Variables required
 var varOne = [];
@@ -10,6 +11,31 @@ var varOne = [];
 
 // Functions ---------------------------------------------------------
 function init() {
+    $('#currentTime').text(moment().format("dddd, Do MMMM YYYY hh:mm a"));
+    setInterval(function () {
+        // var tBlock = $('#currentTime');
+        $('#currentTime').text(moment().format("dddd, Do MMMM YYYY hh:mm a"));
+    }, 1000 * 60); //fires every minute, does not need to stop
+}
+
+$('.saveBtn').click(function () {
+    // var thisId = $('.saveBtn').id ;
+    var thisId = $(this).attr('id');
+    var thisRow = $(this).parent().attr('id');
+    var rowNo = thisRow[thisRow.length - 1];
+
+    var formText = $('#FormControlText' + rowNo).val();
+    var thisRowData = {
+        thisRow : formText ,
+}
+console.log((thisRowData))
+})
+
+function formToLocal(passedData){
+    var savedFormData = JSON.parse(localStorage.getItem('savedActivities'));
+var thisKey = passedData.key();
+
+
 
 }
 
