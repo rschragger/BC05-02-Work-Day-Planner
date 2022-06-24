e6be590133cec2863e7c93e1ea6cebe0e46e5937
rschragger <80379457+rschragger@users.noreply.github.com>
2022-06-24 12:36:45 +1000

    Learning about maps

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
 //Create variables ---------------------------------------------------------
 //var IDs
 //Time (moment) variables
-var startTimeInt = 9 ; //9am is basis
-var hourInt = 60*60*1000 ; //to make adding an hour clearer
+var startTimeInt = 9; //9am is basis
+var hourInt = 60 * 60 * 1000; //to make adding an hour clearer
 
 //Note below are established here to be global and will be refreshed in functions
 var StartOfDay = moment().startOf('day'); //12am today
-var startTime = moment(StartOfDay + (startTimeInt * hourInt)); 
+var startTime = moment(StartOfDay).add(startTimeInt, 'hours');
 
 
 //Other Variables required
@@ -17,27 +17,6 @@ if (localStorage.getItem('savedActivities') == null) {
 };
 
 // Functions ---------------------------------------------------------
-function init() {
-    $('#currentTime').text(moment().format("dddd, Do MMMM YYYY hh:mm a"));
-    setInterval(function () {
-        // var tBlock = $('#currentTime');
-        $('#currentTime').text(moment().format("dddd, Do MMMM YYYY hh:mm a"));
-    }, 1000 * 60); //fires every minute, does not need to stop
-}
-
-$('.saveBtn').click(function () {
-    var thisId = $(this).attr('id');
-    console.log((thisId));
-
-    // var thisRow = $(this).parent().attr('id');
-    var rowNo = thisId[thisId.length - 1];
-
-    var formText = $('#FormControlText' + rowNo).val();
-    var thisRowData = [rowNo, formText]
-    console.log((thisRowData));
-    formToLocal(thisRowData);
-})
-
 function formToLocal(passedData) {
     var savedFormData = JSON.parse(localStorage.getItem('savedActivities'));
     var rowN = passedData[0];
@@ -57,14 +36,54 @@ function formToLocal(passedData) {
 
 }
 
-function localToForm(){
-    var savedFormData = JSON.parse(localStorage.getItem('savedActivities'));
-    if(savedFormData==null){
+function localToForm() {
+    var savedFormData1 = (JSON.parse(localStorage.getItem('savedActivities')));
+
+    var savedFormData = new Map(Object.entries(savedFormData1));
+
+    if (savedFormData == null) {
         formToLocal('')
-        return
-        }
-        
+        return console.log('no data');
     }
+
+    // for (let rNo in savedFormData) {
+    //     $('#' + rNo).text = savedFormData.get(rNo);
+    // }
+
+    savedFormData.forEach(function (value, key) {
+        // text += key + ' = ' + value + "<br>"
+        // $('#' + key).text = value ;
+var valNo = key[key.length-1];
+ $('#FormControlText'+valNo).val(value)
+    })
+
+    // for(rNo=0 ; rNo < savedFormData.length ; rNo++){
+    //     $('row'+rNo).text = eval('savedFormData.row' + rNo);
+
+
+}
+
+//Events ---------------------------------
+$('.saveBtn').click(function () {
+    var thisId = $(this).attr('id');
+    console.log((thisId));
+
+    // var thisRow = $(this).parent().attr('id');
+    var rowNo = thisId[thisId.length - 1];
+
+    var formText = $('#FormControlText' + rowNo).val();
+    var thisRowData = [rowNo, formText]
+    console.log((thisRowData));
+    formToLocal(thisRowData);
+})
+
+//Initialise ----------------------------
+function init() {
+    $('#currentTime').text(moment().format("dddd, Do MMMM YYYY hh:mm a"));
+    setInterval(function () {
+        // var tBlock = $('#currentTime');
+        $('#currentTime').text(moment().format("dddd, Do MMMM YYYY hh:mm a"));
+    }, 1000 * 60); //fires every minute, does not need to stop
 }
 
 init()
