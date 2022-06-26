8ac8e01c464cdd9ba8929f39faedc8f01453aff7
rschragger <80379457+rschragger@users.noreply.github.com>
2022-06-26 22:59:46 +1000

    Testing branch to make a variable version

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,15 @@ var rowHMTL = `      <!--Start Row---------------->
   </div>
 </div>
 <!--End Row---------------->`
+//do NOT delete above
 
+//Button variables
+var startMinus = document.getElementById('start-1');
+var startPlus = document.getElementById('start+1');
+var startInput = document.getElementById('startTime');
+var rowsMinus = document.getElementById('rows-1');
+var rowsPlus = document.getElementById('rows+1');
+var rowsInput = document.getElementById('rows-input');
 
 //Other Variables required
 var varOne = [];
@@ -73,22 +81,22 @@ function makeRows() {
     $('.time-block').text('');
     //work through tenses-past, present, future
 
-    for (x = 0; x < rowCount; x++) {
-        var i = x + startTimeInt;
-        var tenseI = i;
-        if (i > 23) {
-            i = i - 23;
+    for (var x = 0; x < rowCount; x++) {
+        var iT = x + startTimeInt;
+        var tenseI = iT;
+        if (iT > 23) {
+            iT = iT - 23;
         }
-
-        var newText = rowHMTL.replaceAll('^^', i);
+// console.log('x:' + x + " iT:" + iT + ' sti:' + startTimeInt);
+        var newText = rowHMTL.replaceAll('^^', iT);
         var thisTense = establishTense(tenseI);
         var newText = newText.replaceAll('tense', thisTense);
 
         $('.time-block').append(newText);
         // Add time
-        // var rowTime = moment(startTime).add(i, 'hours');
-        var rowTime = moment(startOfDay).add(i, 'hours');
-        $('#hour' + i).text(rowTime.format('hA'))
+        // var rowTime = moment(startTime).add(iT, 'hours');
+        var rowTime = moment(startOfDay).add(iT, 'hours');
+        $('#hour' + iT).text(rowTime.format('hA'))
     }
     initBtn();
 
@@ -111,13 +119,13 @@ function fireOnHour() {
     var thisTime = moment();
     var endHour = moment(thisTime).endOf('hour');
 
-   //'format('x') ' is Unix milliseconds format (lowercase x is milliseconds) so can be used in interval. 
+    //'format('x') ' is Unix milliseconds format (lowercase x is milliseconds) so can be used in interval. 
     //Had to add a second as it was going off 1 second too soon
-     var msecondsToEnd = moment(endHour).format('x') - moment().format('x') + 1 ;  
+    var msecondsToEnd = moment(endHour).format('x') - moment().format('x') + 1;
     //  msecondsToEnd = msecondsToEnd/60/2 ;//testing
-    console.log('fireOnHour has performed at ' + moment().toString() + ' - '+msecondsToEnd)
+    console.log('fireOnHour has performed at ' + moment().toString() + ' - ' + msecondsToEnd)
     //We aren't using setInterval as the first interval is not going to be the full hour
-    setTimeout(onHourTenseReset, msecondsToEnd );
+    setTimeout(onHourTenseReset, msecondsToEnd);
     // setTimeout(onHourTenseReset, 5000 );
 
 }
@@ -135,7 +143,7 @@ function onHourTenseReset() {
         //https://stackoverflow.com/questions/5553551/jquery-change-class-by-given-ids & 1485647/removing-multiple-classes-jquery
         $('#FormControlText' + i).removeClass('past present future tense').addClass(thisTense)
     }
-    console.log('onHourTenseReset has performed at ' + moment().toString())
+    // console.log('onHourTenseReset has performed at ' + moment().toString())
     // fireOnHour()
 }
 
@@ -145,13 +153,38 @@ function setCurrentTime() {
         $('#currentTime').text(moment().format("dddd, Do MMMM YYYY h:mm a"));
         // console.log('currentTime() has performed at '+ moment().toString())
 
-        if(checkTime != moment().format('h')){
+        if (checkTime != moment().format('h')) {
             onHourTenseReset();
-             checkTime = moment().format('h')
+            checkTime = moment().format('h')
         }
     }, 1000 * 60); //fires every minute, does not need to stop
 
 }
+
+function noOfRowsLocal() {
+    var noOfRows = localStorage.getItem('noOfRows');
+    if (noOfRows != null) {
+        window.startTimeInt = noOfRows;
+    } else {
+        noOfRows = startTimeInt;
+    }
+    startInput.value = noOfRows;
+}
+
+function btnMinusPlus(message) {
+    var mesType = message[0];
+    var mesQty = message[1] + message[2];
+    // console.log(mesQty);
+    if (mesType == 's') { //start time
+        startInput.value = parseFloat(startInput.value) + parseFloat(mesQty);
+        startTimeInt = parseFloat(startInput.value) ;
+    } else if (mesType == 'r') { //row qty
+        rowsInput.value = parseFloat(rowsInput.value) + parseFloat(mesQty);
+        rowCount = parseFloat(rowsInput.value);
+    }
+    makeRows()
+}
+
 
 //Events ---------------------------------
 function initBtn() {
@@ -172,10 +205,15 @@ function initBtn() {
 
 //Initialise ----------------------------
 function init() {
+    //Set the number of rows and 
+
+    //Set the current time
     $('#currentTime').text(moment().format("dddd, Do MMMM YYYY h:mm a"));
 
     var endMin = moment().endOf('minute');
-    var msecondsToEnd = moment(endMin).format('x') - moment().format('x'); //lowercase x is milliseconds
+    var msecondsToEnd = moment(endMin).format('x') - moment().format('x') + 10;
+    //Extra 10ms is so that it always fires just after clock strikes the minute
+    //lowercase x is milliseconds
 
     setTimeout(function () {
         $('#currentTime').text(moment().format("dddd, Do MMMM YYYY h:mm a"));
